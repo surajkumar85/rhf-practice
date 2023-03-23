619db8792359e91b992d9d6a74192b4e8259f527
surajkumar85 <suraj17054209@gmail.com>
2023-03-23 16:10:53 +0530

    added react-hook-form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,46 @@
+"use client";
 import styles from "./page.module.css";
+import { useForm, SubmitHandler, UseFormRegister } from "react-hook-form";
+
+type Input = {
+  email: string;
+  password: string;
+};
 
 export default function page() {
-  return <div className={styles.main}>Login form</div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Input>();
+  const onSubmit: SubmitHandler<Input> = (data) => console.log(data);
+
+  return (
+    <div className={styles.main}>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Inputs label={"email"} register={register} required={true} />
+        {errors.email && <p>This field is required</p>}
+        <Inputs label={"password"} register={register} required={true} />
+        {errors.password && <p>This field is required</p>}
+        <button>Submit</button>
+      </form>
+      <button onClick={() => reset()}>Reset</button>
+    </div>
+  );
 }
+type Labels = "email" | "password";
+
+type Inputs = {
+  label: Labels;
+  register: UseFormRegister<Input>;
+  required: boolean;
+};
+
+const Inputs = ({ label, register, required }: Inputs) => (
+  <label>
+    <span>{label}</span>
+    <input {...register(label, { required: required })} />
+  </label>
+);
--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,5 +1,59 @@
+"use client";
 import styles from "./page.module.css";
+import { useForm, SubmitHandler, UseFormRegister, Path } from "react-hook-form";
+
+interface FieldType {
+  email: string;
+  password: string;
+  "c-password": string;
+}
+type Label = "Email" | "Password" | "Confirm Password";
+
+type InputProp = {
+  label: Label;
+  register: UseFormRegister<FieldType>;
+  required: boolean;
+  regLabel: Path<FieldType>;
+  type: string;
+};
+
+const Input = ({ label, register, required, regLabel, type }: InputProp) => (
+  <label>
+    <span>{label}</span>
+    <input type={type} {...register(regLabel, { required: required })} />
+  </label>
+);
 
 export default function page() {
-  return <div className={styles.main}>Registration Form</div>;
+  const { register, handleSubmit } = useForm<FieldType>();
+  const onSubmit: SubmitHandler<FieldType> = (data) => console.log(data);
+  return (
+    <div className={styles.main}>
+      <h1>Registration Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          regLabel="email"
+          label={"Email"}
+          register={register}
+          required={true}
+          type="email"
+        />
+        <Input
+          type="password"
+          regLabel="password"
+          label={"Password"}
+          register={register}
+          required={true}
+        />
+        <Input
+          type="password"
+          regLabel="c-password"
+          label={"Confirm Password"}
+          register={register}
+          required={true}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
